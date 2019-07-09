ea1dbf4991ca5c3a2fd6a8da5b030799e31eb2f5
bitcamp03 <bitcamp03@bitcamp>
2019-07-09 15:17:10 +0900

    Sample controller doAdmin @PreAuthorize("hasRole('ROLE_ADMIN') && principal.username==#uid")

--- security/src/main/java/org/zerock/controller/SampleController.java
+++ security/src/main/java/org/zerock/controller/SampleController.java
@@ -1,8 +1,10 @@
 package org.zerock.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.log4j.Log4j;
 
@@ -22,9 +24,19 @@ public class SampleController {
 	    log.info("logined member");
 	  }
 	  
+	  @PreAuthorize("hasRole('ROLE_ADMIN') && principal.username==#uid")
 	  @GetMapping("/admin")
-	  public void doAdmin() {
+	  public void doAdmin(@RequestParam("uid") String uid) {
 	    
+		log.info("UID : "+ uid );  
 	    log.info("admin only");
 	  }  
+	  
+	  @PreAuthorize("hasRole('ROLE_MEMBER')")
+	  @GetMapping("/manage")
+	  public void doManager() {
+	    
+	    log.info("manage, admin only");
+	  }  
+	  
 }
--- security/src/main/webapp/WEB-INF/views/sample/manager.jsp
+++ security/src/main/webapp/WEB-INF/views/sample/manager.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ taglib uri="http://www.springframework.org/security/tags" prefix="sec" %>
+    <%@ taglib uri="http://www.springframework.org/security/tags" prefix="sec" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,11 +8,11 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h1>all</h1>
-<sec:authorize access="isAnomymous()">
-LOGIN PLZ
-</sec:authorize>
+<h1>Manager Page</h1>
 
+<sec:authorize access="hasRole('ROLE_ADMIN')">
+Admin only
+</sec:authorize>
 
 </body>
 </html>
